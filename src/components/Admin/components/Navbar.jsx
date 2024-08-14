b48496519777d2ccd30efc892c45a956a7826311
Ankit Mittal <ankitmittal0814@gmail.com>
2024-08-14 21:29:05 +0530

    Navigate crci11 to home

--- src/components/Admin/components/Navbar.jsx
+++ src/components/Admin/components/Navbar.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-
-const Navbar = () => (
-  <div className="fixed top-0 left-0 w-full bg-gray-900 p-3 flex flex-col z-10">
-    <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        <h1 className="text-3xl font-bold">Cric11</h1>
+import { useNavigate } from "react-router-dom";
+const Navbar = () => {
+  const navigate = useNavigate();
+  const cric11 = () => {
+    navigate("/");
+  };
+  return (
+    <div className="fixed top-0 left-0 w-full bg-gray-900 p-3 flex flex-col z-10">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <h1
+            className="text-3xl font-bold cursor-pointer"
+            onClick={() => cric11()}
+          >
+            Cric11
+          </h1>
+        </div>
+        <div className="flex-1 mx-20">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-35 p-1 px-2 rounded bg-gray-700 text-white placeholder-gray-400 outline-none"
+          />
+        </div>
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH4dcYWVFHFsz8M3Rsjpy2Hg6gQAmgbCIwWA&s"
+            alt="Admin Profile"
+            className="w-10 h-10 rounded-full object-cover"
+          />
+        </div>
       </div>
-      <div className="flex-1 mx-20">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-35 p-1 px-2 rounded bg-gray-700 text-white placeholder-gray-400 outline-none"
-        />
-      </div>
-      <div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH4dcYWVFHFsz8M3Rsjpy2Hg6gQAmgbCIwWA&s"
-          alt="Admin Profile"
-          className="w-10 h-10 rounded-full object-cover"
-        />
-      </div>
+      <hr className="border-gray-400 mt-3" />
     </div>
-    <hr className="border-gray-400 mt-3" />
-  </div>
-);
-
+  );
+};
 export default Navbar;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import WalletBalance from "./WalletBalance";
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
+  const navigate = useNavigate();
+  const cric11 = () => {
+    navigate("/");
+  };
   return (
     <nav className="bg-red-600 p-4 fixed top-0 left-0 w-full border-b-2 border-gray-400">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-xl font-bold">Cric11</div>
+        <div
+          className="text-white text-xl font-bold cursor-pointer"
+          onClick={() => cric11()}
+        >
+          Cric11
+        </div>
         <div className="font-medium flex items-center">
           <NavLink
             to="/"
