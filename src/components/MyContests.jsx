1a33d97aba83ea5d84c047f97c14fc6fbd81d884
Amit Kumar <ankitmittal0814@gmail.com>
2025-02-23 20:55:48 +0530

    update es-lint

--- src/components/MyContests.jsx
+++ src/components/MyContests.jsx
@@ -1,100 +1,105 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const UserTeams = () => {
-  const [contests, setContests] = useState([]);
-  useEffect(() => {
-    const fetchContests = async () => {
-      const accessToken = localStorage.getItem("accessToken");
-      const response = await axios.get(
-        "http://localhost:8000/api/v1/user-contest/all",
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      // console.log(response.data.data);
+    const [contests, setContests] = useState([]);
+    useEffect(() => {
+        const fetchContests = async () => {
+            const accessToken = localStorage.getItem('accessToken');
+            const response = await axios.get(
+                'http://localhost:8000/api/v1/user-contest/all',
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                },
+            );
+            // console.log(response.data.data);
 
-      setContests(response.data.data);
-    };
-    fetchContests();
-  }, []);
+            setContests(response.data.data);
+        };
+        fetchContests();
+    }, []);
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6 text-gray-600">My Contests</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {contests.length === 0 ? (
-          <div className="flex justify-center col-span-full">
-            <h1 className="text-2xl font-bold text-gray-800 text-center mt-10">
-              No Contests Found...
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-6 text-gray-600">
+                My Contests
             </h1>
-          </div>
-        ) : (
-          contests.map((contest) => (
-            <Link
-              to={`/my-contests/${contest._id}`}
-              key={contest._id}
-              className="mb-2 p-4 rounded-lg shadow-md border-2 border-gray-400 bg-gray-100 hover:bg-gray-200"
-            >
-              <h2 className="text-xl font-bold text-center text-gray-600 mb-1 ">
-                {contest.matchDetails.name}
-              </h2>
-              <div className="flex justify-between mt-4 mb-1">
-                {contest.matchDetails.teamBImg ? (
-                  <img
-                    src={contest.matchDetails.teamBImg}
-                    alt="A"
-                    className="your-css-class h-9"
-                  />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {contests.length === 0 ? (
+                    <div className="flex justify-center col-span-full">
+                        <h1 className="text-2xl font-bold text-gray-800 text-center mt-10">
+                            No Contests Found...
+                        </h1>
+                    </div>
                 ) : (
-                  <p></p>
+                    contests.map((contest) => (
+                        <Link
+                            to={`/my-contests/${contest._id}`}
+                            key={contest._id}
+                            className="mb-2 p-4 rounded-lg shadow-md border-2 border-gray-400 bg-gray-100 hover:bg-gray-200"
+                        >
+                            <h2 className="text-xl font-bold text-center text-gray-600 mb-1 ">
+                                {contest.matchDetails.name}
+                            </h2>
+                            <div className="flex justify-between mt-4 mb-1">
+                                {contest.matchDetails.teamBImg ? (
+                                    <img
+                                        src={contest.matchDetails.teamBImg}
+                                        alt="A"
+                                        className="your-css-class h-9"
+                                    />
+                                ) : (
+                                    <p></p>
+                                )}
+                                <p className="text-center  text-xs text-red-600 font-bold">
+                                    {contest.matchDetails.date
+                                        .split('-')
+                                        .reverse()
+                                        .join('-')}
+                                    <br></br>
+                                    {contest.matchDetails.startTime}
+                                </p>
+                                {contest.matchDetails.teamAImg ? (
+                                    <img
+                                        src={contest.matchDetails.teamAImg}
+                                        alt="B"
+                                        className="your-css-class h-9"
+                                    />
+                                ) : (
+                                    <p></p>
+                                )}
+                            </div>
+                            <div className="flex justify-between">
+                                <p className="text-black mt-4">
+                                    Prize Pool:{' '}
+                                    <span className="font-semibold text-xl">
+                                        ₹{contest.contestDetails.prizePool}
+                                    </span>
+                                </p>
+                                <p className="text-white mt-4 bg-green-600 px-3 py-1 rounded-md">
+                                    Entry:{' '}
+                                    <span className="font-medium">
+                                        ₹{contest.contestDetails.entryFee}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p className="text-black mt-4">
+                                    Spots:{' '}
+                                    <span className="font-semibold">
+                                        {contest.contestDetails.maxParticipants}
+                                    </span>
+                                </p>
+                            </div>
+                        </Link>
+                    ))
                 )}
-                <p className="text-center  text-xs text-red-600 font-bold">
-                  {contest.matchDetails.date.split("-").reverse().join("-")}
-                  <br></br>
-                  {contest.matchDetails.startTime}
-                </p>
-                {contest.matchDetails.teamAImg ? (
-                  <img
-                    src={contest.matchDetails.teamAImg}
-                    alt="B"
-                    className="your-css-class h-9"
-                  />
-                ) : (
-                  <p></p>
-                )}
-              </div>
-              <div className="flex justify-between">
-                <p className="text-black mt-4">
-                  Prize Pool:{" "}
-                  <span className="font-semibold text-xl">
-                    ₹{contest.contestDetails.prizePool}
-                  </span>
-                </p>
-                <p className="text-white mt-4 bg-green-600 px-3 py-1 rounded-md">
-                  Entry:{" "}
-                  <span className="font-medium">
-                    ₹{contest.contestDetails.entryFee}
-                  </span>
-                </p>
-              </div>
-              <div className="flex justify-between">
-                <p className="text-black mt-4">
-                  Spots:{" "}
-                  <span className="font-semibold">
-                    {contest.contestDetails.maxParticipants}
-                  </span>
-                </p>
-              </div>
-            </Link>
-          ))
-        )}
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default UserTeams;
--- src/components/MyContestsDetails.jsx
+++ src/components/MyContestsDetails.jsx
@@ -1,184 +1,206 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const UserContestDetails = () => {
-  const { id } = useParams();
-  const [contest, setContest] = useState(null);
-  const [players, setPlayers] = useState([]);
+    const { id } = useParams();
+    const [contest, setContest] = useState(null);
+    const [players, setPlayers] = useState([]);
 
-  useEffect(() => {
-    const fetchContestDetails = async () => {
-      const accessToken = localStorage.getItem("accessToken");
-      const response = await axios.post(
-        "http://localhost:8000/api/v1/user-contest/get",
-        {
-          id,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
+    useEffect(() => {
+        const fetchContestDetails = async () => {
+            const accessToken = localStorage.getItem('accessToken');
+            const response = await axios.post(
+                'http://localhost:8000/api/v1/user-contest/get',
+                {
+                    id,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                },
+            );
 
-      const { userId, contestId } = response.data.data[0];
+            const { userId, contestId } = response.data.data[0];
 
-      const matchDateAndTime = `${response.data.data[0].matchDetails.date}T${response.data.data[0].matchDetails.startTime}`;
-      const date = new Date();
-      const offsetIST = 5.5 * 60 * 60 * 1000;
-      const istTime = new Date(date.getTime() + offsetIST);
-      const currentDateAndTime = istTime.toISOString().slice(0, 19);
+            const matchDateAndTime = `${response.data.data[0].matchDetails.date}T${response.data.data[0].matchDetails.startTime}`;
+            const date = new Date();
+            const offsetIST = 5.5 * 60 * 60 * 1000;
+            const istTime = new Date(date.getTime() + offsetIST);
+            const currentDateAndTime = istTime.toISOString().slice(0, 19);
 
-      //FIXME:if : match started or ended
-      // if (1) {
-      // if (currentDateAndTime >= matchDateAndTime) {
-      // setTimeout : run api after every 5 minutes
-      //api call for match score
-      //update api
-      // }
-      const res = await axios.post(
-        "http://localhost:8000/api/v1/opponent/get",
-        {
-          contestId: contestId,
-          user_id: userId,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      // console.log(res.data.data);
-      const opponentUserId = res.data.data.opponent;
-      console.log(opponentUserId);
+            //FIXME:if : match started or ended
+            // if (1) {
+            // if (currentDateAndTime >= matchDateAndTime) {
+            // setTimeout : run api after every 5 minutes
+            //api call for match score
+            //update api
+            // }
+            console.log('User id: ', userId);
+            console.log('ContestId: ', contestId);
+            // const res = await axios.post(
+            //     'http://localhost:8000/api/v1/opponent/get',
+            //     {
+            //         contestId: contestId,
+            //         user_id: userId,
+            //     },
+            //     {
+            //         headers: {
+            //             Authorization: `Bearer ${accessToken}`,
+            //         },
+            //     },
+            // );
+            // // console.log(res.data.data);
+            // const opponentUserId = res.data.data.opponent;
+            // console.log(opponentUserId);
 
-      setContest(response.data.data[0]);
+            setContest(response.data.data[0]);
 
-      setPlayers(response.data.data[0].user11);
-    };
+            setPlayers(response.data.data[0].user11);
+        };
 
-    fetchContestDetails();
-  }, [id]);
+        fetchContestDetails();
+    }, [id]);
 
-  if (!contest) return <p>Loading...</p>;
+    if (!contest) return <p>Loading...</p>;
 
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-10 text-gray-600 text-center">
-        {contest.matchDetails.name}
-      </h1>
-      {/* <p className="mb-4 text-gray-700">{contest.description}</p> */}
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2 p-4">
-          <h2 className="text-2xl font-bold">Contest Details:</h2>
-          <p className="text-black mt-4">
-            Match Type:{" "}
-            <span className="font-semibold uppercase text-blue-600">
-              {contest.matchDetails.matchType}
-            </span>
-          </p>
-          <p className="text-black mt-4">
-            Date:{" "}
-            <span className="font-semibold text-orange-600">
-              {contest.matchDetails.date.split("-").reverse().join("-")}
-            </span>
-          </p>
-          <p className="text-black mt-4">
-            Start Time(IST):{" "}
-            <span className="font-semibold text-orange-600">
-              {contest.matchDetails.startTime}
-            </span>
-          </p>
-          <p className="text-black mt-4">
-            Prize Pool:{" "}
-            <span className="font-semibold text-red-600">
-              ₹{contest.contestDetails.prizePool}
-            </span>
-          </p>
-          <p className="text-black mt-4">
-            Entry:{" "}
-            <span className="font-semibold text-green-600">
-              ₹{contest.contestDetails.entryFee}
-            </span>
-          </p>
-          <p className="text-black mt-4">
-            Spots:{" "}
-            <span className="font-semibold text-red-600">
-              {contest.contestDetails.maxParticipants}
-            </span>
-          </p>
-          <p className="text-black mt-4  text-xl">
-            Rank: <span className="font-semibold text-blue-700">{7}</span>
-          </p>
-        </div>
-        <div className="md:w-1/2">
-          <h2 className="text-xl font-bold text-center mb-1 text-green-500">
-            Total Points: <span className="text-3xl text-green-600">{728}</span>
-          </h2>
-          <div className="  bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-green-500 p-6 rounded-lg w-full max-w-lg">
-              <div className="flex justify-center gap-24 mt-8 mb-7">
-                {players.slice(0, 2).map((player) => {
-                  return (
-                    <div key={player._id} className="text-center">
-                      <FontAwesomeIcon
-                        icon={faUser}
-                        className="text-green-800 text-3xl"
-                      />{" "}
-                      {player._id === contest.captain && (
-                        <span className="text-sm text-black font-semibold">
-                          (C)
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-10 text-gray-600 text-center">
+                {contest.matchDetails.name}
+            </h1>
+            {/* <p className="mb-4 text-gray-700">{contest.description}</p> */}
+            <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/2 p-4">
+                    <h2 className="text-2xl font-bold">Contest Details:</h2>
+                    <p className="text-black mt-4">
+                        Match Type:{' '}
+                        <span className="font-semibold uppercase text-blue-600">
+                            {contest.matchDetails.matchType}
                         </span>
-                      )}
-                      {player._id === contest.viceCaptain && (
-                        <span className="text-sm font-semibold text-black">
-                          (VC)
+                    </p>
+                    <p className="text-black mt-4">
+                        Date:{' '}
+                        <span className="font-semibold text-orange-600">
+                            {contest.matchDetails.date
+                                .split('-')
+                                .reverse()
+                                .join('-')}
                         </span>
-                      )}
-                      <span className="block text-white px-6 rounded-sm py-px bg-red-600 text-sm">
-                        {player.name}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-
-              {[2, 5, 8].map((startIdx, index) => (
-                <div key={index} className="grid grid-cols-3 gap-20 mt-16 ">
-                  {players.slice(startIdx, startIdx + 3).map((player) => {
-                    return (
-                      <div key={player._id} className="text-center">
-                        <FontAwesomeIcon
-                          icon={faUser}
-                          className="text-green-800 text-3xl"
-                        />{" "}
-                        {player._id === contest.captain && (
-                          <span className="text-sm text-black font-semibold">
-                            (C)
-                          </span>
-                        )}
-                        {player._id === contest.viceCaptain && (
-                          <span className="text-sm font-semibold text-black">
-                            (VC)
-                          </span>
-                        )}
-                        <span className="block text-white rounded-sm py-px bg-red-600 text-sm">
-                          {player.name}
+                    </p>
+                    <p className="text-black mt-4">
+                        Start Time(IST):{' '}
+                        <span className="font-semibold text-orange-600">
+                            {contest.matchDetails.startTime}
                         </span>
-                      </div>
-                    );
-                  })}
+                    </p>
+                    <p className="text-black mt-4">
+                        Prize Pool:{' '}
+                        <span className="font-semibold text-red-600">
+                            ₹{contest.contestDetails.prizePool}
+                        </span>
+                    </p>
+                    <p className="text-black mt-4">
+                        Entry:{' '}
+                        <span className="font-semibold text-green-600">
+                            ₹{contest.contestDetails.entryFee}
+                        </span>
+                    </p>
+                    <p className="text-black mt-4">
+                        Spots:{' '}
+                        <span className="font-semibold text-red-600">
+                            {contest.contestDetails.maxParticipants}
+                        </span>
+                    </p>
+                    <p className="text-black mt-4  text-xl">
+                        Rank:{' '}
+                        <span className="font-semibold text-blue-700">{7}</span>
+                    </p>
                 </div>
-              ))}
-              <div className="mt-10"></div>
+                <div className="md:w-1/2">
+                    <h2 className="text-xl font-bold text-center mb-1 text-green-500">
+                        Total Points:{' '}
+                        <span className="text-3xl text-green-600">{728}</span>
+                    </h2>
+                    <div className="  bg-opacity-50 flex items-center justify-center z-50">
+                        <div className="bg-green-500 p-6 rounded-lg w-full max-w-lg">
+                            <div className="flex justify-center gap-24 mt-8 mb-7">
+                                {players.slice(0, 2).map((player) => {
+                                    return (
+                                        <div
+                                            key={player._id}
+                                            className="text-center"
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faUser}
+                                                className="text-green-800 text-3xl"
+                                            />{' '}
+                                            {player._id === contest.captain && (
+                                                <span className="text-sm text-black font-semibold">
+                                                    (C)
+                                                </span>
+                                            )}
+                                            {player._id ===
+                                                contest.viceCaptain && (
+                                                <span className="text-sm font-semibold text-black">
+                                                    (VC)
+                                                </span>
+                                            )}
+                                            <span className="block text-white px-6 rounded-sm py-px bg-red-600 text-sm">
+                                                {player.name}
+                                            </span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+
+                            {[2, 5, 8].map((startIdx, index) => (
+                                <div
+                                    key={index}
+                                    className="grid grid-cols-3 gap-20 mt-16 "
+                                >
+                                    {players
+                                        .slice(startIdx, startIdx + 3)
+                                        .map((player) => {
+                                            return (
+                                                <div
+                                                    key={player._id}
+                                                    className="text-center"
+                                                >
+                                                    <FontAwesomeIcon
+                                                        icon={faUser}
+                                                        className="text-green-800 text-3xl"
+                                                    />{' '}
+                                                    {player._id ===
+                                                        contest.captain && (
+                                                        <span className="text-sm text-black font-semibold">
+                                                            (C)
+                                                        </span>
+                                                    )}
+                                                    {player._id ===
+                                                        contest.viceCaptain && (
+                                                        <span className="text-sm font-semibold text-black">
+                                                            (VC)
+                                                        </span>
+                                                    )}
+                                                    <span className="block text-white rounded-sm py-px bg-red-600 text-sm">
+                                                        {player.name}
+                                                    </span>
+                                                </div>
+                                            );
+                                        })}
+                                </div>
+                            ))}
+                            <div className="mt-10"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default UserContestDetails;
