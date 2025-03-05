7203228483d79c5280c313ca286dd83ca1b7be0a
ankitmittal4 <ankitmittal0814@gmail.com>
2025-03-05 18:41:26 +0530

    Animate loading screen

--- src/components/ContestDetails.jsx
+++ src/components/ContestDetails.jsx
@@ -143,7 +143,12 @@ const ContestDetails = () => {
         fetchContestDetails();
     }, [id]);
 
-    if (!contest) return <p>Loading...</p>;
+    if (!contest)
+        return (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                <div className="w-10 h-10 border-4 border-gray-300 border-t-white rounded-full animate-spin"></div>
+            </div>
+        );
 
     return (
         <div className="container mx-auto p-4">
