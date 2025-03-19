bf1226403665c3f6347cedc134b3595659eaaa2f
ankitmittal4 <ankitmittal0814@gmail.com>
2025-03-19 11:08:58 +0530

    Integrate Api in walletbalance component

--- src/components/WalletBalance.jsx
+++ src/components/WalletBalance.jsx
@@ -1,33 +1,41 @@
 // src/components/WalletBalance.js
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWallet } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import { API_URL } from '../../Constants';
+
 const WalletBalance = () => {
-  const [balance, setBalance] = useState(0);
+    const [balance, setBalance] = useState(0);
+    const accessToken = localStorage.getItem('accessToken');
+    useEffect(() => {
+        const fetchBalance = async () => {
+            // Simulate API call to fetch balance
+            const response = await axios.get(`${API_URL}/users/get-balance`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            setBalance(response.data.data.walletBalance);
+        };
 
-  useEffect(() => {
-    const fetchBalance = async () => {
-      // Simulate API call to fetch balance
-      const fetchedBalance = await new Promise((resolve) =>
-        setTimeout(() => resolve(1000), 1000)
-      );
-      setBalance(fetchedBalance);
-    };
+        fetchBalance();
 
-    fetchBalance();
+        // Optional: Set up an interval to periodically update balance
+        const interval = setInterval(fetchBalance, 5000);
 
-    // Optional: Set up an interval to periodically update balance
-    const interval = setInterval(fetchBalance, 5000);
+        return () => clearInterval(interval);
+    }, []);
 
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div className="text-white ml-16 font-bold">
-      <FontAwesomeIcon icon={faWallet} className="text-white text-2xl" />{" "}
-      <span className="">₹{balance}</span>
-    </div>
-  );
+    return (
+        <div className="text-white ml-16 font-bold">
+            <FontAwesomeIcon
+                icon={faWallet}
+                className="text-white text-2xl"
+            />{' '}
+            <span className="">₹{balance}</span>
+        </div>
+    );
 };
 
 export default WalletBalance;
