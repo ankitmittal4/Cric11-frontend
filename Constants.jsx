5fbd102456a7423bd7a283a620397153e0d21210
ankitmittal4 <ankitmittal0814@gmail.com>
2025-03-31 00:07:59 +0530

    Update Api url

--- Constants.jsx
+++ Constants.jsx
@@ -1,4 +1,4 @@
-// const API_URL = 'https://cric11-backend.onrender.com/api/v1';
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://cric11-backend.onrender.com/api/v1';
+// const API_URL = 'http://localhost:8000/api/v1';
 
 export { API_URL };
