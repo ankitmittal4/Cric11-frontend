315b124fca5127abab7029536bf714bb6e87509a
Amit Kumar <k.amitkumar305@gmail.com>
2025-04-18 16:00:42 +0530

    Remove comment

--- src/components/ContestDetails.jsx
+++ src/components/ContestDetails.jsx
@@ -39,7 +39,7 @@ const ContestDetails = () => {
             }
             return prev;
         });
-        // console.log('!!!! -> ', selectedPlayerIds);
+        
     };
 
     const handleCaptainChange = (playerId) => {
