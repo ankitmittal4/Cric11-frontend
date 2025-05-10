36bf87c67a1ab8cf27807579d66ecec425a09cd2
ankitmittal4 <ankitmittal0814@gmail.com>
2025-05-10 21:56:01 +0530

    Update loader in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
 
 
 
-    if (!matches)
+    if (!matches.length)
         return (
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                 <div className="w-10 h-10 border-4 border-gray-300 border-t-white rounded-full animate-spin"></div>
