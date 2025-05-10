ca7cfd392d34d4b0fe848546c6243c2167c683aa
ankitmittal4 <ankitmittal0814@gmail.com>
2025-05-10 21:54:41 +0530

    Update loader

--- src/components/ContestsInMatches.jsx
+++ src/components/ContestsInMatches.jsx
@@ -74,7 +74,7 @@ const ContestsInMatches = () => {
         hours = hours % 12 || 12;
         return `${hours}:${minutes} ${period}`;
     }
-    if (!contests)
+    if (!contests.length)
         return (
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                 <div className="w-10 h-10 border-4 border-gray-300 border-t-white rounded-full animate-spin"></div>
