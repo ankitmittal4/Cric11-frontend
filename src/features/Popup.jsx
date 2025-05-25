66180268e3d43f74697a629af93e469c5de583d5
ankitmittal4 <ankitmittal0814@gmail.com>
2025-05-25 15:18:35 +0530

    Update font for popup message

--- src/features/Popup.jsx
+++ src/features/Popup.jsx
@@ -7,16 +7,16 @@ const Popup = ({ message, onClose }) => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg text-center">
                 <img
-                    className="h-12 w-12 text-center mx-auto"
+                    className="sm:h-12 sm:w-12 h-10 w-10 text-center mx-auto"
                     src={check}
                     alt="Check"
                 ></img>
-                <p className="text-lg text-gray-700 font-semibold mb-4 mt-2 ">
+                <p className="text-sm sm:text-lg text-gray-700 font-semibold mb-4 mt-2 ">
                     {message}
                 </p>
                 <button
                     onClick={onClose}
-                    className="bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600"
+                    className="bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600 sm:text-base text-sm"
                 >
                     Close
                 </button>
