66f6865b55ec0ff2b8fd8c61f3befcfca313b42a
ankitmittal4 <ankitmittal0814@gmail.com>
2025-08-09 22:44:46 +0530

    Enhance error handling in CreateContest component by clearing error messages after 1.5 seconds and adjusting error message display

--- src/Admin/components/CreateContest.jsx
+++ src/Admin/components/CreateContest.jsx
@@ -30,6 +30,9 @@ const CreateContest = () => {
 
         if (entryFee <= 0 || prizePool <= 0 || prizePool <= entryFee || prizePool > entryFee * 2) {
             setErrorMessage('Entry Fee and Prize Pool must be valid numbers.');
+            setTimeout(() => {
+                setErrorMessage('');
+            }, 1500);
             setLoading(false);
             return;
         }
@@ -142,12 +145,15 @@ const CreateContest = () => {
                         />
                     </div>
                     */}
-                    {errorMessage && (
-                        <div className="text-red-500">{errorMessage}</div>
-                    )}
+                    <div className="text-red-500 min-h-6">
+                        {errorMessage && (
+                            <div className="text-red-500 ">{errorMessage}</div>
+                        )}
+
+                    </div>
                     <button
                         type="submit"
-                        className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded-md"
+                        className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded-md "
                     >
                         Create Contest
                     </button>
