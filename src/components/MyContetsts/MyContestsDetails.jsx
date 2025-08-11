fd3e7ac9c18752db5cf4ce513aa750ae2ea4f74f
ankitmittal4 <ankitmittal0814@gmail.com>
2025-08-11 18:10:19 +0530

    Update C and VC positions

--- src/components/MyContetsts/MyContestsDetails.jsx
+++ src/components/MyContetsts/MyContestsDetails.jsx
@@ -759,12 +759,12 @@ const UserContestDetails = () => {
                                             <div key={player.id} className="text-center">
                                                 <div className="relative">
                                                     {player.id === contest.captain && (
-                                                        <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 sm:left-2 -top-4 text-sm text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-4' : 'sm:-left-[.4rem] -left-3'}`}>
+                                                        <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 -top-4 text-sm text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-4' : '-left-2'}`}>
                                                             C
                                                         </span>
                                                     )}
                                                     {player.id === contest.viceCaptain && (
-                                                        <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 sm:left-2 -top-4 text-xs text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-3' : 'sm:-left-[.4rem] -left-1'}`}>
+                                                        <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 -top-4 text-xs text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-3' : '-left-2'}`}>
                                                             VC
                                                         </span>
                                                     )}
@@ -1075,13 +1075,13 @@ const UserContestDetails = () => {
                                                         <div className="relative">
                                                             {captainId ===
                                                                 player.id && (
-                                                                    <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 sm:left-2 -top-4 text-sm text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-4' : 'sm:-left-[.4rem] -left-3'}`}>
+                                                                    <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 -top-4 text-sm text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-4' : 'sm:-left-2 -left-3'}`}>
                                                                         C
                                                                     </span>
                                                                 )}
                                                             {viceCaptainId ===
                                                                 player.id && (
-                                                                    <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 sm:left-2 -top-4 text-xs text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-3' : 'sm:-left-[.4rem] -left-1'}`}>
+                                                                    <span className={`flex items-center justify-center w-6 h-6 absolute sm:-top-3 -top-4 text-xs text-white font-medium bg-gray-500 p-1 rounded-full z-10 ${player.playerImg === "https://h.cricapi.com/img/icon512.png" ? '-left-3' : 'sm:-left-2 -left-1'}`}>
                                                                         VC
                                                                     </span>
                                                                 )}
