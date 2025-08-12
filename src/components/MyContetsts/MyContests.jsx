68dc4624269b081ca4059937190b907725ad62f3
ankitmittal4 <ankitmittal0814@gmail.com>
2025-08-12 13:36:20 +0530

    Update padding in my contests

--- src/components/MyContetsts/MyContests.jsx
+++ src/components/MyContetsts/MyContests.jsx
@@ -63,7 +63,7 @@ const UserTeams = () => {
 
                 <div className="">
                     {loading ? (
-                        <div className="container mx-auto p-4">
+                        <div className="container mx-auto py-4 px-0">
 
                             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                                 {[1, 2, 3, 4, 5].map((_, index) => (
