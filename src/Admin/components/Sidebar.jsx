9eab4e395c5e0f7b52ac00949e3a9991d5fafe64
ankitmittal4 <ankitmittal0814@gmail.com>
2025-09-02 18:43:24 +0530

    Refactor Sidebar and Users components for improved user navigation and data fetching

--- src/Admin/components/Sidebar.jsx
+++ src/Admin/components/Sidebar.jsx
@@ -4,11 +4,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => (
   <div className="fixed top-16 left-0 w-1/5 bg-gray-800 p-5 h-[calc(100vh-4rem)] ">
     <ul className="text-2xl">
-      {/* <li className="mb-3">
-        <Link to="" className="block p-1 px-3 rounded-lg hover:bg-gray-600">
-          Users
-        </Link>
-      </li> */}
+
       <li className="mb-3">
         <Link
           to="matches"
@@ -23,6 +19,11 @@ const Sidebar = () => (
           className="block p-1 px-3 rounded-lg hover:bg-gray-600"
         >
           Contests
+        </Link>
+      </li>
+      <li className="mb-3">
+        <Link to="users" className="block p-1 px-3 rounded-lg hover:bg-gray-600">
+          Users
         </Link>
       </li>
       {/* <li className="mb-3">
--- src/Admin/components/Users.jsx
+++ src/Admin/components/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL;
 // import dummyUsers from "../dummyUsers";
 
 const Users = () => {
@@ -18,42 +19,46 @@ const Users = () => {
         }
     }, []);
     // const fetchUsers = async (page) => {
-    //   setLoading(true);
-    //   try {
-    //     const response = await axios.get("url");
-    //     setUsers(response.data.users);
-    //     setCurrentPage(response.data.currentPage);
-    //     setTotalPages(response.data.totalPages);
-    //   } catch (error) {
-    //     console.log("Error while fetching users: ", error);
-    //   }
-    //   setLoading(false);
+    //     setLoading(true);
+
     // };
 
-    // const fetchUsers = (page) => {
-    //   setLoading(true);
-    //   // Simulate API call with dummy data
-    //   setTimeout(() => {
-    //     const totalUsers = dummyUsers.users.length;
-    //     const startIndex = (page - 1) * limit;
-    //     const endIndex = startIndex + limit;
-    //     const paginateUsers = dummyUsers.users.slice(startIndex, endIndex);
-    //     // const { users, currentPage, totalPages } = dummyUsers;
-    //     setUsers(paginateUsers);
-    //     // setCurrentPage(currentPage);
-    //     setTotalPages(Math.ceil(totalUsers / limit));
-    //     setLoading(false);
-    //   }, 500);
-    // };
-    // useEffect(() => {
-    //   fetchUsers(currentPage);
-    // }, [currentPage]);
+    const fetchUsers = async (page) => {
+        setLoading(true);
+        // Simulate API call with dummy data
+        try {
+            const data = {
+                page: page,
+                limit: limit,
+            }
+            const res = await axios.post(
+                `${API_URL}/admin/all-users`,
+                data,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('adminAccessToken')}`,
+                    },
+                },
+            );
+            console.log("users: ", res.data.data);
 
-    // const handlePageChange = (page) => {
-    //   if (page > 0 && page <= totalPages) {
-    //     setCurrentPage(page);
-    //   }
-    // };
+            setTotalPages(res.data.data.pagination.totalPages)
+            const paginateUsers = res.data.data.users;
+            setUsers(paginateUsers);
+            setLoading(false);
+        } catch (error) {
+            console.log("Error while fetching users: ", error);
+        }
+    };
+    useEffect(() => {
+        fetchUsers(currentPage);
+    }, [currentPage]);
+
+    const handlePageChange = (page) => {
+        if (page > 0 && page <= totalPages) {
+            setCurrentPage(page);
+        }
+    };
     return (
         <>
             <h2 className="text-2xl font-bold mb-5">All Cric Users</h2>
@@ -67,24 +72,28 @@ const Users = () => {
                                 <tr className="text-left ">
                                     <th className="py-2 text-xl px-4 border-b">Name</th>
                                     <th className="py-2 text-xl border-b">Email</th>
+                                    <th className="py-2 text-xl border-b">Username</th>
                                     <th className="py-2 text-xl border-b">Added On</th>
+                                    <th className="py-2 text-xl border-b text-center">Balance</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {users.map((user) => (
-                                    <tr key={user.id} className="text-lg ">
-                                        <td className="py-2 px-4">{user.name}</td>
+                                    <tr key={user._id} className="text-lg ">
+                                        <td className="py-2 px-4">{user.fullName}</td>
                                         <td className="py-2">{user.email}</td>
+                                        <td className="py-2">{user.username}</td>
                                         <td className="py-2">
-                                            {new Date(user.addedOn).toLocaleDateString()}
+                                            {new Date(user.createdAt).toLocaleDateString()}
                                         </td>
+                                        <td className="py-2 text-center">{user.walletBalance}</td>
                                     </tr>
                                 ))}
                             </tbody>
                         </table>
                         <div className="flex justify-between items-center mt-5">
                             <button
-                                // onClick={() => handlePageChange(currentPage - 1)}
+                                onClick={() => handlePageChange(currentPage - 1)}
                                 disabled={currentPage === 1}
                                 className="px-4 py-2 ml-10 mb-8 mt-6 bg-gray-600 rounded disabled:opacity-50 "
                             >
@@ -94,7 +103,7 @@ const Users = () => {
                                 Page {currentPage} of {totalPages}
                             </span>
                             <button
-                                // onClick={() => handlePageChange(currentPage + 1)}
+                                onClick={() => handlePageChange(currentPage + 1)}
                                 disabled={currentPage === totalPages}
                                 className="px-4 py-2 mr-10 mb-8 mt-6 bg-gray-600 rounded disabled:opacity-50"
                             >
